d095a3d25514f108496f5b69c1cbe25ff90e3612
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-08 16:35:57 +0200

    Tiedosto profile.php lisätty

---
+++ profile.php
@@ -0,0 +1,67 @@
+<?php
+// Aloitetaan sessio
+session_start();
+// Jos ei ole kirjautunus, ohjataan kirjautumissivulle
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: membership.html');
+	exit;
+}
+$DATABASE_HOST = 'db';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = 'password';
+$DATABASE_NAME = 'phplogin';
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+// Haetaan käyttäjän tiedot tietokannasta.
+$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+// Haetaan tiedot id:llä
+$stmt->bind_param('i', $_SESSION['id']);
+$stmt->execute();
+$stmt->bind_result($password, $email);
+$stmt->fetch();
+$stmt->close();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Profile Page</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>Website Title</h1>
+                <!-- Linkit uloskirjautumiseen ja etusivulle -->
+                <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+				<a href="home.php"><i class="fas fa-sign-out-alt"></i>Etusivulle</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Profile Page</h2>
+			<div>
+				<p>Your account details are below:</p>
+				<table>
+					<tr>
+						<td>Username:</td>
+						<td><?=$_SESSION['name']?></td>
+					</tr>
+					<tr>
+						<td>Password:</td>
+						<td><?=$password?></td>
+					</tr>
+					<tr>
+						<td>Email:</td>
+						<td><?=$email?></td>
+					</tr>
+				</table>
+				<div>
+                    <!-- Päivitetään tiedot update_form.html -tiedostossa -->
+				<a href="update_form.html">Päivitä tiedot</a> 
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
