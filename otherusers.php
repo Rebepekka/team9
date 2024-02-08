bb3aebe02393cab78bf12282c49cdbbb479fd1f9
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-08 16:28:56 +0200

    Tiedosto otherusers.php lisätty

---
+++ otherusers.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'trtkp23_9';
+$DATABASE_PASS = 'VPnXTtqa';
+$DATABASE_NAME = 'web_trtkp23_9 ';
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+// Tarkistetaan, onko käyttäjä kirjautunut sisään
+if (!isset($_SESSION['loggedin'])) {
+	exit('Please log in first!');
+}
+
+// Haetaan kaikki käyttäjät tietokannasta
+if ($stmt = $con->prepare('SELECT username FROM accounts')) {
+	$stmt->execute();
+	$stmt->bind_result($username);
+	while ($stmt->fetch()) {
+		echo $username . '<br>';
+	}
+	$stmt->close();
+} else {
+	echo 'Could not prepare statement!';
+}
+
+$con->close();
+?>
