2d4a0c63b5b041a7f280067ea35c851d5ea5ba7a
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-08 16:26:53 +0200

    Vaihdettu linkin nimi muut käyttäjät -> other users

--- home.php
+++ home.php
@@ -23,7 +23,7 @@ if (!isset($_SESSION['loggedin'])) {
 				<h1>Website Title</h1>
 				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a> 
 				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-				<a href="muutkayttajat.php"><i class="fas fa-user-circle"></i>Muut käyttäjät</a>
+				<a href="otherusers.php"><i class="fas fa-user-circle"></i>Other users</a>
 			</div>
 		</nav>
 		<div class="content">
