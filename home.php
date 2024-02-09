cf2e7695ec03c260bc8f4fa1d60c51d3779ca2d8
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-09 16:48:51 +0200

    Headerin css:t siirretty homestyle.ccs:stä home.php:n

--- home.php
+++ home.php
@@ -15,7 +15,28 @@ if (!isset($_SESSION['loggedin'])) {
         <link href="css/homestyle.css" rel="stylesheet" type="text/css" />
 		<title>Home Page</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
-    </head>
+    <style>
+	/* Headerin css:t ovat tässä, koska eivät toimi erillisestä tiedostosta käsin */
+	header { /* koko yläosa */
+	/*  position: relative;  elementinsijainti */
+	background-color: black; /* taustaväri */
+	display: flex; /* tehdään elementistä flex */
+	flex-direction: column; /* flex asettelu on sarake (pystysuora) */
+	align-items: center; /* tasataan elementit keskeltä pystysuunnassa*/
+	justify-content: center; /* tasataan elementit keskeltä vaakasuunnassa*/
+	font-weight: bolder; /* otsikon alatekstin fontin leveys paksu */
+	font-size: 30px; /* otsikon alatekstin fonttikoko */
+	margin-top: 1%; /* asetetaan marginaali */
+	margin-left: 10%; /* asetetaan marginaali */
+	margin-right: 10%; /* asetetaan marginaali */
+	color: whitesmoke; /* tekstin väri */
+	}
+	header img { /* yläosan kuva */
+	max-height: 50%; /* max-korkeus headerin korkeudesta*/
+	max-width: 50%; /* max-leveys headerin leveydestä */
+	  }
+	</style>
+	</head>
 	<body class="loggedin">
 		<nav class="navtop">
 			<div>
@@ -34,7 +55,7 @@ if (!isset($_SESSION['loggedin'])) {
 			<li><a href="supplements.html">Supplements</a></li>
 			<li><a href="programs.html">Programs</a></li>
 			<li><a href="apply.html">Apply for coaching</a></li>
-			<li><a href="membership1.html">Membership</a></li>
+			<li><a href="membership.html">Membership</a></li>
 			</ul>
 		</div>
 		<div class="container"> <!--Sisältö. Luokat "container", "row" ja "image" sisältävät omat css:t homestyle.css-tiedostossa-->
