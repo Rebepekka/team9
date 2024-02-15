6b1102e6c0567a6f9948b17f80753b1247588bc9
HetaEkb <heta23005@student.hamk.fi>
2024-02-15 21:29:54 +0200

    kommentteja

--- administrator.php
+++ administrator.php
@@ -1,10 +1,11 @@
 <?php
+// Aloitetaan sessio
 session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'trtkp23_9';
 $DATABASE_PASS = 'VPnXTtqa';
 $DATABASE_NAME = 'web_trtkp23_9';
-// Muodostetaan yhteys tietokantaan, jos yhteyttä ei ole näyrrää error
+// Muodostetaan yhteys tietokantaan, jos yhteyttä ei ole näyttää error
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
