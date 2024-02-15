3ac3dfda32eaa964e03c15e54e7cff57a08d87ae
HetaEkb <heta23005@student.hamk.fi>
2024-02-15 13:57:24 +0200

    Update administrator.php

--- administrator.php
+++ administrator.php
@@ -10,21 +10,48 @@ if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-// Onko käyttäjä ylläpitäjä, jos = "false" 
+// Käyttäjien hakeminen tietokannasta
+function getAllUsers() {
+    global $con;
+
+    $users = array();
+
+    // Query, tietokannassa tietojen saamiseksi 
+    $query = "SELECT id, username, email FROM users";
+    $result = mysqli_query($con, $query);
+
+    // Toimiko?
+    if ($result) {
+        // JOkaiselta riviltä 
+        while ($row = mysqli_fetch_assoc($result)) {
+            $users[] = $row;
+        }
+    }
+    return $users;
+}
+
+// Onko käyttäjä ylläpitäjä 
 function isAdmin() {
     global $con;
+
     if (!isset($_SESSION['username'])) {
         return false; 
     } else { 
+        // Onko käyttäjä ylläpitäjä? 
         $user_name = $_SESSION['username'];
         $sql = "SELECT is_admin FROM users WHERE username = ?";
         $stmt = $con->prepare($sql);
+
+        // Sido parametrit ja suori se
         $stmt->bind_param("s", $user_name);
         $stmt->execute();
         $stmt->bind_result($is_admin);
+
+        // Hakee lopputuloksen 
         $stmt->fetch();
+
         return $is_admin == 1;
-     }
+    }
 }
 
 // Katsotaan onko käyttäjä kirjautunut sisään 
