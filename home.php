31524303972b520860b2e89614bc3994589ac831
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-29 20:52:13 +0200

    Uutta sisältöä

--- home.php
+++ home.php
@@ -3,79 +3,125 @@
 session_start();
 // Jos käyttäjä ei ole kirjautunut sisään, ohjataan kirjautumissivulle
 if (!isset($_SESSION['loggedin'])) {
-	header('Location: membership.html');
+	header('Location: index.html');
 	exit;
 }
 ?>
 <!-- Kirjautumisen jälkeen käyttäjä tulee tälle sivulle -->
 <!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-        <link href="css/homestyle.css" rel="stylesheet" type="text/css" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
-        <title>Home Page</title>
-	</head>
-	<body>
-	<header> <!-- Sivun yläosa (kuva ja musta tausta) -->
-    	<img src="images/etusivu_isokuva.jpg" title="Some title" alt="Gym">
-	</header>		
-		<nav class="navtop">
-			<div>
-                <!-- Linkit profiilitietoihin, uloskirjautumiseen ja muiden käyttäjien selaamiseen -->
-				<h1>Welcome, <?=$_SESSION['name']?>!</h1>
-				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a> 
-				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-				<a href="otherusers.php"><i class="fas fa-users"></i>Other users</a>
-			</div>
-		</nav>
-		<div class="eka">
-		<p>From the menu below, you can find our products and services</p>
-			<ul>
-			<li><a href="gymwear.html">Gymwear</a></li>
-			<li><a href="accessories.html">Accessories</a></li>
-			<li><a href="supplements.html">Supplements</a></li>
-			<li><a href="programs.html">Programs</a></li>
-			<li><a href="apply.html">Apply for coaching</a></li>
-			<li><a href="membership.html">Membership</a></li>
-			</ul>
-		</div>
-		<div class="container"> <!--Sisältö. Luokat "container", "row" ja "image" sisältävät omat css:t homestyle.css-tiedostossa-->
-  <div class="row">
-    <div class="text">All accessories for the gym. By clicking below the text, you can explore our selection.<br><br><strong><a href="accessories.html">Accessories</a></strong>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Bodybuilding and training programs. Membership, memberpage.">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link href="css/mainstyle.css" rel="stylesheet" type="text/css" /> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <title>Bodybuilders | Memberpage</title>
+    <style>
+.headline {
+    background-image: url(images/etusivu_isokuva.jpg);
+}       
+.headline2 {
+    background-image: url(images/painot.jpg);
+}
+.headline3 {
+    background-image: url(images/gym1.jpg);
+}
+.client {
+    background-image: url(images/heart.jpg);
+}  
+.client2 {
+    background-image: url(images/thumb.jpg);
+}
+footer {
+    background-image: url(images/trainee.jpg);
+}
+.fa-star {
+    display: none;
+}
+/* Näyttää tähden hiiren ollessa tekstin päällä */
+.text:hover .fa-star {
+    display: inline-block;
+    color: gold;
+}
+    </style>
+</head>
+<body>
+    <header>
+        <a href="index.html"><h1>BODYBUILDERS</h1></a>
+    </header>
+    <div class="headline">
+    <p>WELCOME,<br><?=$_SESSION['name']?>!</p>
     </div>
-    <img class="image" src="images/accessories.jpg" alt="Accessories">
-  </div>
-  <div class="row">
-    <div class="text">Supplements to support your training. By clicking below the text, you can explore our selection.<br><br><strong><a href="supplements.html">Supplements</a></strong>
+<!-- Linkit profiilitietoihin, uloskirjautumiseen ja muiden käyttäjien selaamiseen -->
+  <div class="container">
+          <nav class="navbar navbar-expand-xl navbar-dark bg-black">        
+            <div class="container-fluid">
+              <ul class="navbar-nav mr-auto">  
+                <li class="nav-item active">
+                  <a class="nav-link" href="profile.php" style="font-size: 19px; filter: brightness(95%);">PROFILE</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="otherusers.php" style="font-size: 19px; filter: brightness(95%);">OTHER USERS</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="logout.php" style="font-size: 19px; filter: brightness(85%); color: gold;">LOG OUT</a>
+                </li>
+              </ul>
+            </div>
+          </nav>   
+  <main>
+    <div class="headline2">
+            <p><strong>IN THE SECTION BELOW YOU WILL FIND OUR PRODUCTS</strong></p>
     </div>
-    <img class="image" src="images/whey.jpg" alt="Creatine">
-  </div>
-  <div class="row">
-    <div class="text">The latest fashion for the gym. By clicking below the text, you can explore our selection.<br><br><strong><a href="gymwear.html">Gymwear</a></strong>
+    <div class="section">
+        <div class="row">
+        <img class="img-responsive" src= "images/accessories1.jpg" alt="gym accessories">
+        <div class="text-box"><a href="accessories.html">ACCESSORIES</a></div>
+        <div class="text"><a href="accessories.html">ALL ACCESSORIES FOR THE GYM</a>
+        </div>
+        </div>
+        <div class="row">
+        <img class="img-responsive" src="images/creatine1.jpg" alt="creatine">
+        <div class="text-box"><a href="supplements.html">SUPPLEMENTS</a></div>
+        <div class="text"><a href="supplements.html">SUPPLEMENTS TO SUPPORT YOUR TRAINING</a>
+        </div>
+        </div>
+        <div class="row">
+        <img class="img-responsive" src="images/gymwear.jpg" alt="gymwear">
+        <div class="text-box"><a href="gymwearmember.html">GYMWEAR</a></div>
+        <div class="text"><a href="gymwearmember.html">THE LATEST FASHION FOR THE GYM</a>
+        </div>
+        </div>
     </div>
-    <img class="image" src="images/RedShoes.jpg" alt="Grey Shoes">
-  </div>
-  <div class="row">
-    <div class="text">Workout programs developed by professionals. By clicking below the text, you can explore our selection.<br><br><strong><a href="programs.html">Programs</a></strong>
-    </div>
-    <img class="image" src="images/programs.jpg" alt="Oversize shirt">
-  </div>
-  <div class="row">
-    <div class="text">We have several coaches at our gym. By clicking below the text, you can get to know our coaches.<br><br><strong><a href="apply.html">Apply for coaching</a></strong>
-    </div>
-    <img class="image" src="images/valmennus.jpg" alt="OIP">
-  </div>
-  <div class="row">
-    <div class="text">Join us as a loyal customer. By clicking below the text, you can explore the benefits of being a loyal customer.<br><br><strong><a href="membership.html">Membership</a></strong>
-    </div>
-    <img class="image" src="images/membership.jpg" alt="OIP">
-  </div>
+<div class="headline3">
+        <p><strong>IN THE SECTION BELOW YOU WILL FIND OUR SERVICES</strong></p>
 </div>
-<footer> <!--Sivun alaosa -->
-  <p>Bodybuilders @ 2024</p>
-  <address><p>Pääskynpolku 313</p><br><p>12345 Pääskysaari</p><br><p>Puhelin: 010 010 010</p>
-  </address>
+<div class="section">
+    <div class="row">
+    <img class="img-responsive" src= "images/programs1.jpg" alt="gym programs">
+    <div class="text-box"><a href="programs.html">PROGRAMS</a></div>
+    <div class="text"><a href="programs.html">WORKOUT PROGRAMS</a>
+    </div>
+    </div>
+    <div class="row">
+    <img class="img-responsive" src="images/coaching.jpg" alt="coaching">
+    <div class="text-box"><a href="personal_training.html">APPLY FOR COACHING</a></div>
+    <div class="text"><a href="personal_training.html">GET TO KNOW OUR COACHES</a>
+    </div>
+    </div>
+    <div class="row">
+    <img class="img-responsive" src="images/vip.jpg" alt="gym membership">
+    <div class="text-box"><a href="gymwearmember.html">VIP TIP: CHECK OUT OUR GYMWEAR PAGE!</a></div>
+    <div class="text"><a class="show-stars">YOU ARE <i class="fa-regular fa-star"></i> A STAR</a>
+    </div>
+    </div>
+</div>
+</main>
+</div>
+<footer>
+    <address>BODYBUILDERS<br>Punttikuja 313<br>12345 PUNTTILA</address>
 </footer>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 	</body>
 </html>    
