bc03514bac12495d1ed2b263cc4602d505483e99
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-29 20:57:39 +0200

    Sisältö päivitetty

--- otherusers.php
+++ otherusers.php
@@ -4,24 +4,21 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="css/otherusersstyle.css" rel="stylesheet" type="text/css" />
-	<title>Document</title>
+	<title>Bodybuilders | Other users</title>
 </head>
 <body>
 	<header></header>
+
 	<div class="text">
-		<h1>Other users</h1>
+		<h1>OTHER USERS</h1>
 	</div>
 
 	<div class="users">
 
 <?php
 session_start();
-$initials=parse_ini_file("./.ht.settings.ini");
 // Muodostetaan yhteys.
-$con=mysqli_connect($initials["host"], $initials["user"], $initials["pass"], $initials["name"]);
-if (mysqli_connect_errno()) {
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
+include ("./connect.php"); // Linkki: luodaan yhteys.
 // Tarkistetaan, onko käyttäjä kirjautunut sisään
 if (!isset($_SESSION['loggedin'])) {
 	exit('Please log in first!');
